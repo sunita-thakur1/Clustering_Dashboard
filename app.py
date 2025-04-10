0a918ae6bf6399dc365e43787d0c402b06da2f43
sunita-thakur1 <67176792+sunita-thakur1@users.noreply.github.com>
2025-04-10 01:41:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -132,6 +132,7 @@ if uploaded_file:
         # Show labeled data
         st.subheader("📊 Clustered Data Preview")
         st.dataframe(df.head())
+        st.subheader("📊 Clustered Data")
         st.write(df)
 
     else:
